4b8410d13870e16bd1294412571a208bcf76a3d0
Young5416 <1101744096@qq.com>
2019-06-03 17:07:49 +0800

    feat(底部导航栏添加): 基础样式,点击效果
    新增底部导航组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './demo/draw_demo.dart';
+import './demo/bottom_navigation_bar_demo.dart';
+import './demo/listview_demo.dart';
 
 void main() => runApp(App());
 
@@ -18,53 +20,52 @@ class App extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-            title: Text('Young'),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.search),
-                tooltip: 'search',
-                onPressed: () => 'search button is pressed',
-              )
-            ],
-            elevation: 10.0,
-            centerTitle: true,
-            //标题是否居中，默认为false
-            bottom: TabBar(
-                unselectedLabelColor: Colors.black38,
-                indicatorColor: Colors.black54,
-                indicatorSize: TabBarIndicatorSize.tab,
-                indicatorWeight: 1.0,
-                tabs: <Widget>[
-                  Tab(icon: Icon(Icons.local_activity)),
-                  Tab(icon: Icon(Icons.verified_user)),
-                  Tab(icon: Icon(Icons.g_translate)),
-                ]),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Young'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'search',
+              onPressed: () => 'search button is pressed',
+            )
+          ],
+          elevation: 10.0,
+          centerTitle: true,
+          //标题是否居中，默认为false
+          bottom: TabBar(
+              unselectedLabelColor: Colors.black38,
+              indicatorColor: Colors.black54,
+              indicatorSize: TabBarIndicatorSize.tab,
+              indicatorWeight: 1.0,
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_activity)),
+                Tab(icon: Icon(Icons.verified_user)),
+                Tab(icon: Icon(Icons.g_translate)),
+              ]),
+        ),
+        body: TabBarView(children: <Widget>[
+          ListViewDemo(),
+          Icon(
+            Icons.verified_user,
+            size: 128.0,
+            color: Colors.black12,
           ),
-          body: TabBarView(children: <Widget>[
-            Icon(
-              Icons.local_activity,
-              size: 128.0,
-              color: Colors.black12,
-            ),
-            Icon(
-              Icons.verified_user,
-              size: 128.0,
-              color: Colors.black12,
-            ),
-            Icon(
-              Icons.g_translate,
-              size: 128.0,
-              color: Colors.black12,
-            ),
-          ]),
-          drawer: DrawDemo()),
+          Icon(
+            Icons.g_translate,
+            size: 128.0,
+            color: Colors.black12,
+          ),
+        ]),
+        drawer: DrawDemo(),
+        bottomNavigationBar: BottomNavigationBarDemo(),
+      ),
     );
   }
 }
