9c85f67567d6e00b6e1d4c5e8b7eb4ebdd3d8902
Young5416 <1101744096@qq.com>
2019-06-18 17:45:01 +0800

    feat(按钮部件): 输入模块
    输入模块熟悉(dateTime),引入intl包

--- lib/demo/datetime_demo.dart
+++ lib/demo/datetime_demo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'dart:async';
 
 class DateTimeDemo extends StatefulWidget {
   @override
@@ -6,6 +8,37 @@ class DateTimeDemo extends StatefulWidget {
 }
 
 class _DateTimeDemoState extends State<DateTimeDemo> {
+  DateTime selectedDate = DateTime.now();
+  TimeOfDay selectedTime = TimeOfDay(hour: 9, minute: 30);
+
+  _selectDate() async {
+    final DateTime date = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2100),
+    );
+
+    if (date == null) return;
+
+    setState(() {
+      selectedDate = date;
+    });
+  }
+
+  _selectTime() async {
+    final TimeOfDay time = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+
+    if (time == null) return;
+
+    setState(() {
+      selectedTime = time;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +51,26 @@ class _DateTimeDemoState extends State<DateTimeDemo> {
         children: <Widget>[
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[],
+            children: <Widget>[
+              InkWell(
+                onTap: _selectDate,
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.arrow_drop_down),
+                    Text(DateFormat.yMMMd().format(selectedDate))
+                  ],
+                ),
+              ),
+              InkWell(
+                onTap: _selectTime,
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.arrow_drop_down),
+                    Text(selectedTime.format(context))
+                  ],
+                ),
+              ),
+            ],
           )
         ],
       ),
